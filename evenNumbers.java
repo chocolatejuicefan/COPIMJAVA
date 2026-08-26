1d21e01fb65e0db2daaef6cbc6a620c7b2e2a68c
Santiago Duarte <venlibduarte2612@gmail.com>
2026-08-26 17:54:35 -0400

    Add files via upload

---
+++ evenNumbers.java
@@ -0,0 +1,9 @@
+public class evenNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            if (i % 2 == 0)  // check if the number is even
+                System.out.println(i);
+            
+        }
+    }
+}
